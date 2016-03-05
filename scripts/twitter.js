dff44219116cf91613b362c0e2fd62ead425c554
67mohamed <67mohamed@gmail.com>
2016-03-05 11:37:15 -0500

    Integrated Twitter

--- scripts/twitter.js
+++ scripts/twitter.js
@@ -1,12 +1,23 @@
 var Twitter = require('twitter');
 
-var client = new Twitter({
-  consumer_key: 'RSgnkUZwGxAWWnqZAWh7QFOt2',
-  consumer_secret: 'JckgGctzqn7Md5rkhUh3VPbnVE4EQSYL4zE0704MtsMFaopplj',
-  access_token_key: '248339629-xt2AKRQttBQr3WEUyiovDKrDeu09bObacg50QpcT',
-  access_token_secret: 'dtNUkxKfbcdY7tihRXpCVfTwxAw6QoLp4PZSq6b10Nq8l'
-});
+var twitterLog = function(){
+	console.log("Lg her")
+	var client = new Twitter({
+	  consumer_key: 'RSgnkUZwGxAWWnqZAWh7QFOt2',
+	  consumer_secret: 'JckgGctzqn7Md5rkhUh3VPbnVE4EQSYL4zE0704MtsMFaopplj',
+	  access_token_key: '248339629-xt2AKRQttBQr3WEUyiovDKrDeu09bObacg50QpcT',
+	  access_token_secret: 'dtNUkxKfbcdY7tihRXpCVfTwxAw6QoLp4PZSq6b10Nq8l'
+	});
  
-client.get('search/tweets', {q: 'node.js'}, function(error, tweets, response){
-   console.log(tweets);
-});
+	client.get('search/tweets', {q: 'node.js'}, function(error, tweets, response){
+	   console.log(tweets);
+	});
+
+	var runTweet = new function(){
+
+		console.log("hey");
+	}
+}
+
+
+module.export = twitterLog;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
+var Twitter = require('twitter');
 var app = express();
 
 
@@ -14,4 +15,22 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
+});
+
+
+var client = new Twitter({
+	  consumer_key: 'RSgnkUZwGxAWWnqZAWh7QFOt2',
+	  consumer_secret: 'JckgGctzqn7Md5rkhUh3VPbnVE4EQSYL4zE0704MtsMFaopplj',
+	  access_token_key: '248339629-xt2AKRQttBQr3WEUyiovDKrDeu09bObacg50QpcT',
+	  access_token_secret: 'dtNUkxKfbcdY7tihRXpCVfTwxAw6QoLp4PZSq6b10Nq8l'
+	});
+
+client.stream('statuses/filter', {track: 'GoldmanSachs'}, function(stream) {
+  stream.on('data', function(tweet) {
+    console.log(tweet.text);
+  });
+
+  stream.on('error', function(error) {
+    throw error;
+  });
 });
